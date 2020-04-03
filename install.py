307cc9891fbb55266fb5b80f7e9f657622525609
Emre K <emre1393@gmail.com>
2020-04-03 19:30:41 +0000

    install.py edited with update option, it will ask link of update zip file. gta released r22c and made updates available to everyone.

--- install.py
+++ install.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import subprocess, os, random, string, sys, shutil, socket
+import subprocess, os, random, string, sys, shutil, socket, zipfile
 from itertools import cycle, izip
+from zipfile import ZipFile
 
 rDownloadURL = {"main": "https://bitbucket.org/emre1393/xtreamui_mirror/downloads/main_xtreamcodes_reborn.tar.gz", "sub": "https://bitbucket.org/emre1393/xtreamui_mirror/downloads/sub_xtreamcodes_reborn.tar.gz", "admin": "https://bitbucket.org/emre1393/xtreamui_mirror/downloads/admin_xtreamcodes_reborn.tar.gz"}
 rPackages = ["libcurl3", "libxslt1-dev", "libgeoip-dev", "e2fsprogs", "wget", "mcrypt", "nscd", "htop", "zip", "unzip", "mc", "python-paramiko", "mysql-server"]
 rInstall = {"MAIN": "main", "LB": "sub", "ADMIN": "admin"}
+rUpdate = {"UPDATE": "update"}
 rMySQLCnf = "IyBYdHJlYW0gQ29kZXMKCltjbGllbnRdCnBvcnQgICAgICAgICAgICA9IDMzMDYKCltteXNxbGRfc2FmZV0KbmljZSAgICAgICAgICAgID0gMAoKW215c3FsZF0KdXNlciAgICAgICAgICAgID0gbXlzcWwKcG9ydCAgICAgICAgICAgID0gNzk5OQpiYXNlZGlyICAgICAgICAgPSAvdXNyCmRhdGFkaXIgICAgICAgICA9IC92YXIvbGliL215c3FsCnRtcGRpciAgICAgICAgICA9IC90bXAKbGMtbWVzc2FnZXMtZGlyID0gL3Vzci9zaGFyZS9teXNxbApza2lwLWV4dGVybmFsLWxvY2tpbmcKc2tpcC1uYW1lLXJlc29sdmU9MQoKYmluZC1hZGRyZXNzICAgICAgICAgICAgPSAqCmtleV9idWZmZXJfc2l6ZSA9IDEyOE0KCm15aXNhbV9zb3J0X2J1ZmZlcl9zaXplID0gNE0KbWF4X2FsbG93ZWRfcGFja2V0ICAgICAgPSA2NE0KbXlpc2FtLXJlY292ZXItb3B0aW9ucyA9IEJBQ0tVUAptYXhfbGVuZ3RoX2Zvcl9zb3J0X2RhdGEgPSA4MTkyCnF1ZXJ5X2NhY2hlX2xpbWl0ICAgICAgID0gNE0KcXVlcnlfY2FjaGVfc2l6ZSAgICAgICAgPSAyNTZNCgoKZXhwaXJlX2xvZ3NfZGF5cyAgICAgICAgPSAxMAptYXhfYmlubG9nX3NpemUgICAgICAgICA9IDEwME0KCm1heF9jb25uZWN0aW9ucyAgPSAyMDAwMApiYWNrX2xvZyA9IDQwOTYKb3Blbl9maWxlc19saW1pdCA9IDIwMjQwCmlubm9kYl9vcGVuX2ZpbGVzID0gMjAyNDAKbWF4X2Nvbm5lY3RfZXJyb3JzID0gMzA3Mgp0YWJsZV9vcGVuX2NhY2hlID0gNDA5Ngp0YWJsZV9kZWZpbml0aW9uX2NhY2hlID0gNDA5NgoKCnRtcF90YWJsZV9zaXplID0gMUcKbWF4X2hlYXBfdGFibGVfc2l6ZSA9IDFHCgppbm5vZGJfYnVmZmVyX3Bvb2xfc2l6ZSA9IDEwRwppbm5vZGJfYnVmZmVyX3Bvb2xfaW5zdGFuY2VzID0gMTAKaW5ub2RiX3JlYWRfaW9fdGhyZWFkcyA9IDY0Cmlubm9kYl93cml0ZV9pb190aHJlYWRzID0gNjQKaW5ub2RiX3RocmVhZF9jb25jdXJyZW5jeSA9IDAKaW5ub2RiX2ZsdXNoX2xvZ19hdF90cnhfY29tbWl0ID0gMAppbm5vZGJfZmx1c2hfbWV0aG9kID0gT19ESVJFQ1QKcGVyZm9ybWFuY2Vfc2NoZW1hID0gMAppbm5vZGItZmlsZS1wZXItdGFibGUgPSAxCmlubm9kYl9pb19jYXBhY2l0eT0yMDAwMAppbm5vZGJfdGFibGVfbG9ja3MgPSAwCmlubm9kYl9sb2NrX3dhaXRfdGltZW91dCA9IDAKaW5ub2RiX2RlYWRsb2NrX2RldGVjdCA9IDAKCgpzcWwtbW9kZT0iTk9fRU5HSU5FX1NVQlNUSVRVVElPTiIKCltteXNxbGR1bXBdCnF1aWNrCnF1b3RlLW5hbWVzCm1heF9hbGxvd2VkX3BhY2tldCAgICAgID0gMTZNCgpbbXlzcWxdCgpbaXNhbWNoa10Ka2V5X2J1ZmZlcl9zaXplICAgICAgICAgICAgICA9IDE2TQo=".decode("base64")
 
 class col:
@@ -14,6 +16,7 @@ class col:
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
+    YELLOW = '\033[33m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
@@ -81,6 +84,33 @@ def install(rType="MAIN"):
         return True
     printc("Failed to download installation file!", col.FAIL)
     return False
+
+def update(rType="MAIN"):
+    printc("Enter the link of release_xyz.zip file:", col.WARNING)
+    rlink = raw_input('Example: https://xtream-ui.com/releases/release_22c.zip\n\nNow enter the link:\n\n')
+    try: rURL = rlink
+    except:
+        printc("Invalid download URL!", col.FAIL)
+        return False
+    print "\n"
+    os.system('wget -q -O "/tmp/update.zip" "%s"' % rURL)
+    if os.path.exists("/tmp/update.zip"):
+        try: is_ok = zipfile.ZipFile("/tmp/update.zip")
+        except:
+            printc("Invalid link or zip file is corrupted!", col.FAIL)
+            os.remove("/tmp/update.zip")
+            return False
+        printc("Downloading Software Update")  
+        printc("Updating Software")
+        os.system('chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb > /dev/null && rm -rf /home/xtreamcodes/iptv_xtream_codes/admin > /dev/null && rm -rf /home/xtreamcodes/iptv_xtream_codes/pytools > /dev/null && unzip /tmp/update.zip -d /tmp/update/ > /dev/null && cp -rf /tmp/update/XtreamUI-master/* /home/xtreamcodes/iptv_xtream_codes/ > /dev/null && rm -rf /tmp/update/XtreamUI-master > /dev/null && rm -rf /tmp/update > /dev/null && chown -R xtreamcodes:xtreamcodes /home/xtreamcodes/ > /dev/null && chmod +x /home/xtreamcodes/iptv_xtream_codes/permissions.sh > /dev/null && chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb > /dev/null')
+        if not "chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/nginx" in open("/home/xtreamcodes/iptv_xtream_codes/permissions.sh").read(): os.system('echo "sudo chmod -R 777 /home/xtreamcodes 2>/dev/null\nsudo find /home/xtreamcodes/iptv_xtream_codes/admin/ -type f -exec chmod 644 {} \; 2>/dev/null\nsudo find /home/xtreamcodes/iptv_xtream_codes/admin/ -type d -exec chmod 755 {} \; 2>/dev/null\nsudo find /home/xtreamcodes/iptv_xtream_codes/wwwdir/ -type f -exec chmod 644 {} \; 2>/dev/null\nsudo find /home/xtreamcodes/iptv_xtream_codes/wwwdir/ -type d -exec chmod 755 {} \; 2>/dev/null\nsudo chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/nginx 2>/dev/null\nsudo chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp 2>/dev/null\nsudo chmod 400 /home/xtreamcodes/iptv_xtream_codes/config 2>/dev/null" > /home/xtreamcodes/iptv_xtream_codes/permissions.sh')
+        os.system("/home/xtreamcodes/iptv_xtream_codes/permissions.sh > /dev/null")
+        try: os.remove("/tmp/update.zip")
+        except: pass
+        return True
+    printc("Failed to download installation file!", col.FAIL)
+    return False
+
 
 def mysql(rUsername, rPassword):
     global rMySQLCnf
@@ -175,7 +205,10 @@ def modifyNginx():
 
 if __name__ == "__main__":
     printc("Xtream Codes Reborn BETA - Installer", col.OKGREEN, 2)
-    rType = raw_input("  Installation Type [MAIN, LB, ADMIN]: ")
+    print "%s │ NOTE: this is a forked mirror of original installer from https://xtream-ui.com/install/install.py %s" % (col.OKGREEN, col.ENDC)
+    print "%s │ Check out the mirror repo: https://bitbucket.org/emre1393/xtreamui_mirror/ %s" % (col.OKGREEN, col.ENDC)
+    print " "
+    rType = raw_input("  Installation Type [MAIN, LB, ADMIN, UPDATE]: ")
     print " "
     if rType.upper() in ["MAIN", "LB"]:
         if rType.upper() == "LB":
@@ -219,4 +252,12 @@ if __name__ == "__main__":
                 printc("Admin UI: http://%s:25500" % getIP())
                 printc("Admin UI default login is admin/admin")
         else: printc("Install Xtream Codes Main first!", col.FAIL)
+    elif rType.upper() == "UPDATE":
+        if os.path.exists("/home/xtreamcodes/iptv_xtream_codes/wwwdir/api.php"):
+            printc("Update Admin Panel? Y or N?", col.WARNING)
+            if raw_input("  ").upper() == "Y":
+                if not update(rType.upper()): sys.exit(1)
+                printc("Installation completed!", col.OKGREEN, 2)
+                start()
+            else: printc("Install Xtream Codes Main first!", col.FAIL)
     else: printc("Invalid installation type", col.FAIL)
